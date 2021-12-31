98ebb742640d266e96a239b7ed53b9ba157304fa
João Amadeu <jmamadeu2000@gmail.com>
2021-12-31 05:46:13 +0100

    chore: add theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  useFonts
+} from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { theme } from "./src/styles/theme";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world...!</Text>
+      <Text style={styles.text}>Hello world...!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.colors.white,
+    backgroundColor: theme.color.white,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
+  text: {
+    ...theme.font.h1
+  }
 });
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,99 @@
-export const theme = {
-  colors: {
-    white: "#fff",
+import { TextStyle } from "react-native";
+
+enum Fonts {
+  h1 = "h1",
+  h2 = "h2",
+  h3 = "h3",
+  p1 = "p1",
+  p2 = "p2",
+  s = "s"
+}
+
+enum Colors {
+  primary = "primary",
+  secondary = "secondary",
+  mainText = "mainText",
+  secondaryText = "secondaryText",
+  outline = "outline",
+  form = "form",
+  white = "white"
+}
+
+type FontsType = {
+  [x in Fonts]: TextStyle;
+};
+
+type ColorsType = {
+  [x in Colors]: string;
+};
+
+type ThemeProps = {
+  font: FontsType;
+  color: ColorsType;
+};
+
+const color: ColorsType = {
+  primary: "#1FCC79",
+  secondary: "#FF6464",
+  mainText: "#2E3E5C",
+  secondaryText: "#9FA5C0",
+  outline: "#D0DBEA",
+  form: "#F4F5F7",
+  white: "#fff"
+};
+
+const font: FontsType = {
+  h1: {
+    fontFamily: "Inter",
+    fontWeight: "bold",
+    fontSize: 22,
+    fontStyle: "normal",
+    lineHeight: 32,
+    letterSpacing: 0.5
   },
+  h2: {
+    fontFamily: "Inter",
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 27,
+    letterSpacing: 0.5,
+    fontSize: 17
+  },
+  h3: {
+    fontFamily: "Inter",
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 25,
+    letterSpacing: 0.5,
+    fontSize: 15
+  },
+  p1: {
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    lineHeight: 27,
+    letterSpacing: 0.5,
+    fontSize: 17,
+    fontWeight: "500"
+  },
+  p2: {
+    fontFamily: "Inter",
+    fontSize: 15,
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: 25,
+    letterSpacing: 0.5
+  },
+  s: {
+    fontFamily: "Inter",
+    fontWeight: "500",
+    fontStyle: "normal",
+    fontSize: 12,
+    lineHeight: 15,
+    letterSpacing: 0.5
+  }
+};
+
+export const theme: ThemeProps = {
+  color,
+  font
 };
