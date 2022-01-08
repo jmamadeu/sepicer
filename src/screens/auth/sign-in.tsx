14f52d6b9cee2ec8b5b50aa75d7db3c2f03b5b30
João Amadeu <jmamadeu2000@gmail.com>
2022-01-08 01:41:36 +0100

    chore: break into a smaller components

--- src/screens/auth/sign-in.tsx
+++ src/screens/auth/sign-in.tsx
@@ -1,48 +1,80 @@
+import GoogleIcon from "@assets/icons/google";
+import { LockIcon } from "@assets/icons/lock";
+import { MessageIcon } from "@assets/icons/message";
 import { Button } from "@atoms/button";
 import { Typography } from "@atoms/typography";
+import { InputText } from "@molecules/input-text";
 import { theme } from "@styles/theme";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const SignIn = (): JSX.Element => (
   <SafeAreaView style={styles.container}>
     <View style={styles.contentContainer}>
       <View style={styles.inputsContentContainer}>
-        <Typography variant="h1">Welcome back</Typography>
-        <Typography style={styles.secondaryTitle} variant="p2">
+        <Typography color={theme.colors.mainText} variant="h1">
+          Welcome back
+        </Typography>
+        <Typography
+          color={theme.colors.secondaryText}
+          style={styles.secondaryTitle}
+          variant="p2"
+        >
           Please enter your account here
         </Typography>
 
-        <TextInput
+        <InputText
           placeholder="Email or phone number"
-          style={styles.input}
-          accessibilityRole="text"
-        />
-        <TextInput
-          placeholder="Password"
-          style={[styles.input, styles.firstInput]}
-          accessibilityRole="text"
+          icon={<MessageIcon color={theme.colors.mainText} />}
         />
 
-        <Typography variant="p2" style={styles.forgotTextStyle}>
+        <InputText
+          placeholder="Email or phone number"
+          containerStyle={styles.inputPasswordStyle}
+          icon={<LockIcon color={theme.colors.mainText} />}
+        />
+
+        <Typography
+          variant="p2"
+          color={theme.colors.mainText}
+          style={styles.forgotTextStyle}
+        >
           Forgot password?
         </Typography>
       </View>
 
       <View style={styles.buttonsContainer}>
         <Button style={styles.button} variant="primary" onPress={() => {}}>
-          <Text>Login</Text>
+          <Text style={styles.buttonText}>Login</Text>
         </Button>
 
-        <Text>Or continue with</Text>
+        <Typography
+          variant="p2"
+          color={theme.colors.mainText}
+          style={styles.continueTextStyle}
+        >
+          Or continue with
+        </Typography>
 
-        <Button style={styles.button} variant="primary" onPress={() => {}}>
-          <Text>Login</Text>
+        <Button
+          style={[styles.button, styles.googleButtonStyles]}
+          variant="primary"
+          onPress={() => {}}
+        >
+          <GoogleIcon style={styles.iconStyes} color={theme.colors.white} />
+          <Text style={styles.buttonText}>Google</Text>
         </Button>
 
-        <Text>
-          Don&apos;t have any account? <Text>Sign Up</Text>
-        </Text>
+        <Typography
+          variant="p2"
+          color={theme.colors.mainText}
+          style={styles.googleTextStyle}
+        >
+          Don&apos;t have any account?{" "}
+          <Typography variant="h3" color={theme.colors.primary}>
+            Sign Up
+          </Typography>
+        </Typography>
       </View>
     </View>
   </SafeAreaView>
@@ -66,28 +98,37 @@ const styles = StyleSheet.create({
     marginTop: 8,
     marginBottom: 32
   },
-  input: {
-    height: 56,
-    borderWidth: 1,
-    padding: 10,
-    width: "100%",
-    borderRadius: 32,
-    paddingLeft: 24,
-    fontSize: 15,
-    borderColor: theme.color.outline
-  },
-  firstInput: {
+  inputPasswordStyle: {
     marginTop: 16
   },
   forgotTextStyle: {
     alignSelf: "flex-end",
-    marginTop: 24,
-    color: theme.color.mainText
+    marginTop: 24
   },
   buttonsContainer: {
     alignItems: "center"
   },
   button: {
     width: "100%"
+  },
+  buttonText: {
+    fontWeight: "700",
+    fontSize: 15,
+    fontFamily: theme.fonts.bold,
+    color: theme.colors.white
+  },
+  googleButtonStyles: {
+    backgroundColor: theme.colors.secondary,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  iconStyes: {
+    marginRight: 10
+  },
+  continueTextStyle: {
+    marginVertical: 24
+  },
+  googleTextStyle: {
+    marginTop: 24
   }
 });
